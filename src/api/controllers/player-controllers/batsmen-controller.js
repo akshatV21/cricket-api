c34da16faf41e1c113370caff090ad70efcfc8c7
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-17 18:57:54 +0530

    added players route tests

--- src/api/controllers/player-controllers/batsmen-controller.js
+++ src/api/controllers/player-controllers/batsmen-controller.js
@@ -1,4 +1,5 @@
 const { BatsmenModel } = require("../../models/player-models/batsmen-model")
+const batter = require("../../../../tests/data/batsmen-data")
 
 const httpGetAllBatsmen = async (req, res) => {
   try {
@@ -8,11 +9,14 @@ const httpGetAllBatsmen = async (req, res) => {
     console.log(error)
     res.status(500).json({ error: "Internal server error!" })
   }
+  // const batsmen = new BatsmenModel(batter)
+  // await batsmen.save()
+  // res.json(batsmen)
 }
 
 const httpGetSingleBatsmen = async (req, res) => {
   try {
-    const batsmen = await BatsmenModel.findOne({ _id: req.params.batsmenId })
+    const batsmen = await BatsmenModel.findById(req.params.batsmenId)
     if (!batsmen) return res.status(400).json({ error: "Batsmen not found!" })
 
     res.status(200).json(batsmen)
@@ -28,11 +32,11 @@ const httpGetSingleBatsmenStats = async (req, res) => {
     if (!batsmen) return res.status(400).json({ error: "Batsmen not found!" })
 
     const playerStats = {}
-    req.format.forEach(format => {
+    req.formats.forEach(format => {
       playerStats[format] = batsmen.stats[format]
     })
 
-    res.status(200).json(playerStats)
+    return res.status(200).json(playerStats)
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: "Internal server error!" })
