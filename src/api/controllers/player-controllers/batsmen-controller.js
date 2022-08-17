1eba21a5a3aed945e468ddd813cbb377eea57067
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-17 12:55:22 +0530

    player: GET /batsmen/stats/:batsmenId

--- src/api/controllers/player-controllers/batsmen-controller.js
+++ src/api/controllers/player-controllers/batsmen-controller.js
@@ -22,4 +22,21 @@ const httpGetSingleBatsmen = async (req, res) => {
   }
 }
 
-module.exports = { httpGetAllBatsmen, httpGetSingleBatsmen }
+const httpGetSingleBatsmenStats = async (req, res) => {
+  try {
+    const batsmen = await BatsmenModel.findOne({ _id: req.params.batsmenId })
+    if (!batsmen) return res.status(400).json({ error: "Batsmen not found!" })
+
+    const playerStats = {}
+    req.format.forEach(format => {
+      playerStats[format] = batsmen.stats[format]
+    })
+
+    res.status(200).json(playerStats)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Internal server error!" })
+  }
+}
+
+module.exports = { httpGetAllBatsmen, httpGetSingleBatsmen, httpGetSingleBatsmenStats }
