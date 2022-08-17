b891546d0a3bd6f503ffef0298bd0cae45173c98
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-17 12:27:45 +0530

    player router added

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 const { Router } = require("express")
+const playerRouter = require("./routes/player-router")
 const rankingsRouter = require("./routes/rankings-router")
 const recordRouter = require("./routes/record-router")
 const teamRouter = require("./routes/team-router")
@@ -6,6 +7,7 @@ const teamRouter = require("./routes/team-router")
 const api = Router()
 
 api.use("/teams", teamRouter)
+api.use("/players", playerRouter)
 api.use("/records", recordRouter)
 api.use("/rankings", rankingsRouter)
 
---
+++ src/api/routes/player-router.js
@@ -0,0 +1,5 @@
+const { Router } = require("express")
+
+const playerRouter = Router()
+
+module.exports = playerRouter
